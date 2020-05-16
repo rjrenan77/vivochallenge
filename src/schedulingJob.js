b6f02925c756fa75a5ffe17e9a2e5cf3eb314cfe
Renan Soares <renansoares@Renans-iMac.local>
2020-05-16 16:44:31 -0300

    validação das datas de execução com as datas de conclusão do job

--- src/schedulingJob.js
+++ src/schedulingJob.js
@@ -1,38 +1,75 @@
 // funcao que recebe um objeto que possui a janela de execucao com data e hora de inicio e fim mais a massa de dados
 // antes de executar a funcao, validar se as datas maximas de conclusão batem com a janela de execução
 // calcular na ordem de datas o prazo maximo de 8 horas para cada array de job retornado
-//exemplo do data
-// [
-//   {
-//   "ID": 1,
-//   "Descrição": "Importação de arquivos de fundos",
-//   "Data Máxima de conclusão": 2019-11-10 12:00:00,
-//   "Tempo estimado": 2 horas,
-//   },
-//   {
-//   "ID": 2,
-//   "Descrição": "Importação de dados da Base Legada",
-//   "Data Máxima de conclusão": 2019-11-11 12:00:00,
-//   "Tempo estimado": 4 horas,
-//   },
-//   {
-//   "ID": 3,
-//   "Descrição": "Importação de dados de integração",
-//   "Data Máxima de conclusão": 2019-11-11 08:00:00,
-//   "Tempo estimado": 6 horas,
-//   },
-//   ]
-//exemplo do executionWindow
-/*
-{
-  startTime: 2019-11-10 09:00:00,
-  endTime: 2019-11-11 12:00:00
-}
 
+const dateFNS = require('date-fns');
+const dateTZ = require('date-fns-tz');
 
-*/
 function jobSchedule(executionWindow, data) {
-    var endTime = executionWindow;
-    console.log(typeof executionWindow);
+
+  const jobsArray = [];
+
+  const parsedToDateStartTime = dateFNS.parseISO(executionWindow.startTime);
+  const znDateStartTime = dateTZ.zonedTimeToUtc(parsedToDateStartTime, 'America/Sao_Paulo');
+
+  const parsedToDateEndTime = dateFNS.parseISO(executionWindow.endTime);
+  const znDateEndTime = dateTZ.zonedTimeToUtc(parsedToDateEndTime, 'America/Sao_Paulo');
+
+  for(let i = 0; i < data.length; i++){
+
+    const parsedToDateDataMaxima = dateFNS.parseISO(data[i].dataMaxima);
+    const znDateDataMaxima = dateTZ.zonedTimeToUtc(parsedToDateDataMaxima, 'America/Sao_Paulo');
+
+    const compareDataMaximaWithStartTime = dateFNS.compareAsc(znDateDataMaxima, znDateStartTime);
+    const compareDataMaximaWithEndTime = dateFNS.compareAsc(znDateDataMaxima, znDateEndTime);
+
+    //dataMaximaDeConclusao é menor do que a data inicial de execução?
+    if(compareDataMaximaWithStartTime === -1){
+     console.log(compareDataMaximaWithStartTime + "dataMaximadeConclusao é menor do que a data inicial de execucao")
+      
+    }
+
+    //dataMaximaDeConclusao é maior do que a data final de execução?
+    if(compareDataMaximaWithEndTime === 1){
+     console.log(compareDataMaximaWithEndTime + "dataMaximadeConclusao é maior do que a data final de execucao")
+    }
+
+    //dataMaximaDeConclusao está entre as datas minimas e maximas de execução?
+    if((compareDataMaximaWithStartTime === 1 || compareDataMaximaWithStartTime === 0) && (compareDataMaximaWithEndTime === -1 || compareDataMaximaWithEndTime === 0)){
+      console.log(compareDataMaximaWithStartTime +" " + compareDataMaximaWithEndTime + "dataMaximadeConclusao enta entre as datas de execucao")
+
+      jobsArray.push(data[i].id);
+
+     
+    }
+    
+  }
+
+  return jobsArray;
+
 }
-jobSchedule({}, []);
+
+
+
+
+console.log(jobSchedule({startTime: '2019-11-10 09:00:00',
+  endTime: '2019-11-11 12:00:00'}, [
+    {
+    "id": 1,
+    "descricao": "Importação de arquivos de fundos",
+    "dataMaxima": '2019-11-10 12:00:00',
+    "tempoEstimado": 2 ,
+    },
+    {
+    "id": 2,
+    "descricao": "Importação de dados da Base Legada",
+    "dataMaxima": '2019-11-11 12:00:00',
+    "tempoEstimado": 4 ,
+    },
+    {
+    "id": 3,
+    "descricao": "Importação de dados de integração",
+    "dataMaxima": '2019-11-11 08:00:00',
+    "tempoEstimado": 6 ,
+    },
+  ]));
